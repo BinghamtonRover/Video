2e5d7d4a8e038102257bd961bf943dc59351ca3d
Levi Lesches <levilesches@gmail.com>
2023-07-19 23:17:19 -0400

    First commit, imshow demo

---
+++ bin/video.dart
@@ -0,0 +1,16 @@
+import "package:opencv_ffi/opencv_ffi.dart";
+
+import 'package:video/video.dart' as video;
+
+void main() async {
+  final camera = Camera(0);
+  try {
+    while (true) {
+      if (!camera.read()) { throw Exception("Camera read failed"); }
+      camera.display();
+      await Future<void>.delayed(Duration(milliseconds: (1000/60).round()));
+    }
+  } finally {
+    camera.dispose();    
+  }
+}
