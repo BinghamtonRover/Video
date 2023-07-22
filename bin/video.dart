c9156ce847d4083ba15e0ddaeb652ccfa39e951d
aidanahram <aidanahram1@gmail.com>
2023-07-22 13:26:15 -0400

    not much added

--- bin/video.dart
+++ bin/video.dart
@@ -1,5 +1,15 @@
 // ignore_for_file: avoid_print
+import "package:opencv_ffi/opencv_ffi.dart";
 
+class VideoCollection{
+  /// Holds a list of available cameras
+  var cameras = List<Camera>;
+
+  void start(){
+    print("Starting Cameras...");
+  }
+}
 void main() {
 	print("The main program has not yet been implemented");
+  final collection = VideoCollection()..start();
 }
