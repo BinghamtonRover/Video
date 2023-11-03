6ad8835d70f5aa6a514a4eac31e6a9d49e60b34b
aidanahram <aidanahram1@gmail.com>
2023-11-02 22:35:45 -0400

    sdaasd sdasd wq
    Revert "made CameraIsolate Class, need to change VideoServer next"
    
    This reverts commit 5199e9f5d38a05e5631ec2b210a6e10f3517e8e8.

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,15 +1,12 @@
-import "package:burt_network/burt_network.dart";
-import "package:video/src/functions.dart";
-
-import 'parent_isolate.dart';
+import "main_isolate.dart";
 import "camera_isolates.dart";
 
 void main() async {
-  final parent = VideoController();
-  final child = await parent.spawn(CameraIsolate(details: getDefaultDetails(CameraName.ROVER_FRONT)));
+  final parent = CameraController();
+  final child = await parent.spawn(FrontIsolate());
   await Future<void>.delayed(const Duration(seconds: 1));
   await parent.run();
-  await Future<void>.delayed(const Duration(minutes: 1), () async {
+  await Future<void>.delayed(const Duration(minutes: 5), () async {
     child.kill();
     await parent.dispose();
   });
