9e9dd668a566324950ae7a6f824ba7a5e34dc18b
Levi Lesches <levilesches@gmail.com>
2023-11-28 16:42:21 -0500

    Fixed a bunch of errors

--- bin/librealsense_example.dart
+++ bin/librealsense_example.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print, prefer_final_locals, cascade_invocations
+
 import "package:video/video.dart";
 import "dart:ffi/ffi.dart";
 import "package:ffi/ffi.dart";
@@ -6,18 +8,15 @@ import "dart:io";
 final arena = Arena();
 
 /// Function calls to librealsense may raise errors of type rs_error*/
-void checkError(Pointer<rs2_error> e)
-{
-    if (e != nullptr)
-    {
-        print("rs_error was raised when calling ${nativeLib.rs2_get_failed_function(e)}(${nativeLib.rs2_get_failed_args(e)})");
-        print("    ${nativeLib.rs2_get_error_message(e)}");
-        exit(0);
-    }
+void checkError(Pointer<rs2_error> e) {
+  if (e != nullptr) {
+    print("rs_error was raised when calling ${nativeLib.rs2_get_failed_function(e)}(${nativeLib.rs2_get_failed_args(e)})");
+    print("    ${nativeLib.rs2_get_error_message(e)}");
+    exit(0);
+  }
 }
 
-
-void main(){
+void main() {
   final errorPtr = arena<Pointer<rs2_error>>();
   
   /// Create a context object. This object owns the handles to all connected realsense devices.
@@ -37,7 +36,7 @@ void main(){
   final Pointer<rs2_device> dev = nativeLib.rs2_create_device(device_list, 0, errorPtr);
   checkError(errorPtr.value);
 
-  //Pointer<uint16_t> one_meter = (uint16_t)(1.0f / get_depth_unit_value(dev));
+  final one_meter = 1.0 / nativeLib.get_depth_unit_value(dev);
 
   /// Create a pipeline to configure, start and stop camera streaming
   final Pointer<rs2_pipeline> pipeline =  nativeLib.rs2_create_pipeline(context, errorPtr);
@@ -94,9 +93,8 @@ void main(){
   final int display_size = (rows + 1) * (row_length + 1);
   final int buffer_size = display_size;
 
-  final Pointer<Char> buffer = calloc(display_size);
+  final Pointer<Char> buffer = arena<Char>(display_size);
   final Pointer<Char> out = nullptr;
-
 
   while(true) {
     // This call waits until a new composite_frame is available
@@ -108,71 +106,67 @@ void main(){
     final int num_of_frames = nativeLib.rs2_embedded_frames_count(frames, errorPtr);
     checkError(errorPtr.value);
 
-    int i = 0;
-    for (i = 0; i < num_of_frames; ++i) {
+    for (int i = 0; i < num_of_frames; ++i) {
       final Pointer<rs2_frame> frame = nativeLib.rs2_extract_frame(frames, i, errorPtr);
       checkError(errorPtr.value);
 
       // Check if the given frame can be extended to depth frame interface
       // Accept only depth frames and skip other frames
-      if (0 == nativeLib.rs2_is_frame_extendable_to(frame, rs2_extension.RS2_EXTENSION_DEPTH_FRAME, errorPtr)){
+      if (0 == nativeLib.rs2_is_frame_extendable_to(frame, rs2_extension.RS2_EXTENSION_DEPTH_FRAME, errorPtr)) {
         nativeLib.rs2_release_frame(frame);
         continue;
       }
 
-      /// Retrieve depth data, configured as 16-bit depth values */
-      final Pointer<void> depth_frame_data = nativeLib.rs2_get_frame_data(frame, errorPtr);
+      /// Retrieve depth data, configured as 16-bit depth values
+      Pointer<void> depth_frame_data = nativeLib.rs2_get_frame_data(frame, errorPtr);
       checkError(errorPtr.value);
 
-          /* Print a simple text-based representation of the image, by breaking it into 10x5 pixel regions and approximating the coverage of pixels within one meter */
+      /* Print a simple text-based representation of the image, by breaking it into 10x5 pixel regions and approximating the coverage of pixels within one meter */
       out.value = buffer.value;
-      int x, y, i;
-      final Pointer<Int> coverage = calloc(row_length, sizeof(int));
+      int x, y;
+      final Pointer<Int> coverage = arena<Int>(row_length);
+      // final Pointer<Int> coverage = calloc(row_length, sizeof(int));
 
-        for (y = 0; y < height.value; ++y){
-          for (x = 0; x < width.value; ++x){
-              // Create a depth histogram to each row
-              final int coverage_index = x ~/ 10;
-              final int depth = depth_frame_data++;
-              if (depth > 0 && depth < one_meter)
-                  ++coverage[coverage_index];
-          }
-
-          if ((y % 10) == (9)){
-              for (i = 0; i < (row_length); ++i)
-              {
-                  static const char* pixels = " .:nhBXWW";
-                  int pixel_index = (coverage[i] / (HEIGHT_RATIO * WIDTH_RATIO / sizeof(pixels)));
-                  *out++ = pixels[pixel_index];
-                  coverage[i] = 0;
-              }
-              *out++ = '\n';
-          }
+      for (y = 0; y < height.value; ++y) {
+        for (x = 0; x < width.value; ++x) {
+          // Create a depth histogram to each row
+          final int coverage_index = x ~/ 10;
+          final int depth = depth_frame_data++;  // <-- This is a void* though?
+          if (depth > 0) ++x;
         }
-        *out++ = 0;
-        printf("\n%s", buffer);
 
-        free(coverage);
-        rs2_release_frame(frame);
+        if ((y % 10) == 9) {
+          for (i = 0; i < row_length; ++i) {
+            const List<String> pixels = [" ", ".", ":", "n", "h", "B", "X", "W", "W"];
+            int pixel_index = (coverage[i] / (HEIGHT_RATIO * WIDTH_RATIO / pixels.length)).truncate();
+            // *out++ = pixels[pixel_index];
+            coverage[i] = 0;
+          }
+          // *out++ = "\n";
+        }
       }
 
-      rs2_release_frame(frames);
+      // *out++ = 0;
+      print("\n$buffer");
+      nativeLib.rs2_release_frame(frame);
+      nativeLib.rs2_release_frame(frames);
+    }
   }
-
   /// Stop the pipeline streaming
-  nativeLib.rs2_pipeline_stop(pipeline, errorPtr);
-  checkError(errorPtr.value);
+  // THIS CODE WON'T RUN BECAUSE IT'S BELOW AN INFINITE LOOP!
 
-  /// Release resources
-  free(buffer);
-  nativeLib.rs2_delete_pipeline_profile(pipeline_profile);
-  nativeLib.rs2_delete_stream_profiles_list(stream_profile_list);
-  nativeLib.rs2_delete_stream_profile(stream_profile);
-  nativeLib.rs2_delete_config(config);
-  nativeLib.rs2_delete_pipeline(pipeline);
-  nativeLib.rs2_delete_device(dev);
-  nativeLib.rs2_delete_device_list(device_list);
-  nativeLib.rs2_delete_context(ctx);
+  // nativeLib.rs2_pipeline_stop(pipeline, errorPtr);
+  // checkError(errorPtr.value);
 
-  arena.releaseAll();
+  // /// Release resources
+  // nativeLib.rs2_delete_pipeline_profile(pipeline_profile);
+  // nativeLib.rs2_delete_stream_profiles_list(stream_profile_list);
+  // nativeLib.rs2_delete_stream_profile(stream_profile);
+  // nativeLib.rs2_delete_config(config);
+  // nativeLib.rs2_delete_pipeline(pipeline);
+  // nativeLib.rs2_delete_device(dev);
+  // nativeLib.rs2_delete_device_list(device_list);
+  // nativeLib.rs2_delete_context(context);
+
+  // arena.releaseAll();
 }
--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,4 +1,4 @@
-import "package:burt_network/burt_network.dart";
+import "package:burt_network/generated.dart";
 
 /// These list maps OpenCV IDs (index) to [CameraName]s.
 ///
