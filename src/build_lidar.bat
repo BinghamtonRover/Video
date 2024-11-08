eaff9bc4f6f0655b175da53474a5f2525a393116
aidan ahram <aidanahram1@gmail.com>
2024-11-08 13:27:05 -0500

    I have done it. I have mastered the art of CMake

--- src/build_lidar.bat
+++ src/build_lidar.bat
@@ -1,6 +1,6 @@
-@echo off
+@REM echo off
 
-@REM cd lidar/sick_scan_xd
+
 @REM # Clone repository sick_scan_xd
 @REM git clone -b master https://github.com/SICKAG/sick_scan_xd.git
 @REM # Build libraries sick_scan_xd_shared_lib.dll
@@ -9,12 +9,12 @@
 @REM set _cmake_string=Visual Studio 17
 @REM set _msvc=Visual Studio 2022
 @REM set _cmake_build_dir=build
-@REM cd sick_scan_xd
+@REM @REM cd lidar/sick_scan_xd
 @REM if not exist %_cmake_build_dir% mkdir %_cmake_build_dir%
 @REM pushd %_cmake_build_dir%
-@REM cmake -DROS_VERSION=0 -G "%_cmake_string%" ..
 @REM if %ERRORLEVEL% neq 0 ( @echo ERROR building %_cmake_string% sick_scan_xd with cmake & @pause )
 @REM cmake --build . --clean-first --config Debug
+@REM @REM cmake --build . --clean-first --config Release
 @REM cd ..\..
 
 rem Build lidar_ffi_wrapper
@@ -25,3 +25,4 @@ if %ERRORLEVEL% == 1 exit /b
 cmake --build .
 cd ..
 copy build\Debug\lidar_ffi.dll ..\dist
+copy build\sick_scan_xd\Debug\sick_scan_xd_shared_lib.dll ..\dist
