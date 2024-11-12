58f00cfd4753d61301f5755e4091e44ecb4897c9
aidan ahram <aidanahram1@gmail.com>
2024-11-11 20:27:05 -0500

    not the master I thought I was

--- src/build_lidar.bat
+++ src/build_lidar.bat
@@ -22,7 +22,7 @@ if not exist build mkdir build
 cd build
 cmake ../lidar
 if %ERRORLEVEL% == 1 exit /b
-cmake --build .
+cmake --build . --clean-first
 cd ../..
 if not exist dist mkdir dist
 cd src
