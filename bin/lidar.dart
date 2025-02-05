899a36acfe61ee846748943c1da2d5facd55d5aa
BinghamtonRover <rover@binghamton.edu>
2025-02-04 22:44:14 -0500

    Added lidar server

---
+++ bin/lidar.dart
@@ -0,0 +1,24 @@
+import "dart:io";
+import "dart:typed_data";
+
+import "package:burt_network/udp.dart";
+
+void main() async {
+	final socket = UdpSocket(port: 8020);
+	await socket.init();
+	socket.stream.listen(temp);
+	while (true) {
+		await Future<void>.delayed(Duration(seconds: 1));
+	}
+}
+
+void temp(Datagram packet) {
+	final data = Float64List.sublistView(packet.data);
+	final processed = [
+		for (final number in data) 
+			if (!number.isNaN)
+				number.toStringAsFixed(3),
+	];
+	print("------------------------------------------");
+	print(processed);
+}
