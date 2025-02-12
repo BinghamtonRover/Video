6a6f1cc75a326ac6a12dd5f7b21f7dc0e0c5826a
Levi Lesches <levilesches@gmail.com>
2025-02-12 03:54:55 -0500

    Use `cmake --build -j$(nproc) .` instead of `make` (#27)

--- build.sh
+++ build.sh
@@ -17,14 +17,8 @@ then
   mkdir -p build
   cd build
   cmake .. -DFORCE_LIBUVC=true -DCMAKE_POSITION_INDEPENDENT_CODE=true -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=true -DBUILD_GLSL_EXTENSIONS=true -DBUILD_TOOLS=true -DBUILD_WITH_OPENMP=true -DBUILD_SHARED_LIBS=true -DCMAKE_BUILD_TYPE=Release
-  if [[ -v CI ]];
-  then
-    echo Running on CI, using all cores
-    make
-  else
-    echo Running on Pi, using 8 cores
-    make -j8
-  fi
+  echo "Building RealSense with $(nproc) processors"
+  cmake --build . -j$(nproc)
   sudo make install
   cd ../../..
 fi
